7ab0d655e1fa4b60c4e976f9b1997cb51bd18262
EC-FIGUEROA <figueroaec@gmail.com>
2026-06-25 17:03:07 +0800

    update

--- src/rest/omms/DatabaseConnection.java
+++ src/rest/omms/DatabaseConnection.java
@@ -37,24 +37,17 @@ public class DatabaseConnection {
          return DriverManager.getConnection(url);
          //return DriverManager.getConnection(url, username, password);
      }
-    public Connection getConnection2() {
-        if (connection2 == null) {
-            try {
-                // Register the PostgreSQL driver
-                Class.forName("org.postgresql.Driver");
+    public Connection getConnection2() throws Exception {
+    	 /**/ String url = System.getenv("DATABASE_URL");
 
-                // Establish the connection
-              //String url = "jdbc:postgresql://172.17.100.6:5432/joblist";
-                String url = "jdbc:postgresql://localhost:5432/joblist";
-                String username = "postgres";
-                String password = "03_0431A";//ileco1_amfm
-                connection2 = DriverManager.getConnection(url, username, password);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return connection2;
+         if (url == null || url.isEmpty()) {
+             throw new RuntimeException("DATABASE_URL is not set");
+         }
+         /** String url = "jdbc:postgresql://172.17.100.6:5432/joblist";
+         String username = "postgres";
+         String password = "03_0431A";//ileco1_amfm /**/
+         Class.forName("org.postgresql.Driver");
+         return DriverManager.getConnection(url);
+         //return DriverManager.getConnection(url, username, password);
     }
 }
