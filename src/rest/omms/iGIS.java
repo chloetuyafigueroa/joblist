5df653f7eac516f0c29942a1a64064b093c7581f
EC-FIGUEROA <figueroaec@gmail.com>
2026-07-22 11:06:31 +0800

    update

--- src/rest/omms/iGIS.java
+++ src/rest/omms/iGIS.java
@@ -38,6 +38,7 @@ public class iGIS extends HttpServlet {
 	/**
 	 * 
 	 */
+	private static final String SYNC_ENABLED = System.getenv("SYNC_ENABLED");
 	private static final long serialVersionUID = 1L;
 	//public static DataSource dataSource = DataSourceConfig.getDataSource();
 	//private static final Logger log = Logger.getLogger( iGIS.class.getName());
@@ -79,9 +80,9 @@ public class iGIS extends HttpServlet {
 		}/**/
     	//smsModule.startListening(); 
     	 //smsModule.startSMSListening();
-		if (executor == null || executor.isShutdown()) {
+		if (executor == null || executor.isShutdown() ||SYNC_ENABLED.equals("TRUE")) {
 	        executor = Executors.newScheduledThreadPool(2);
-	        //ruN();
+	        ruN();
 	    }
 		 
 	}
